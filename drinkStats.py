b24037f986b01b3242c5460959f37e389ab71d83
Jeff Haak <zemon1@csh.rit.edu>
2013-08-30 13:24:14 -0400

    This is literally the slowest subquery of all time, why

--- drinkStats.py
+++ drinkStats.py
@@ -1,1 +1,41 @@
-import creds
+import creds, MySQLdb
+
+
+
+
+
+
+
+
+def uploadDrops(stat, ro):
+     ro.execute("""SELECT * FROM `drop_log` WHERE `username` in (SELECT Distinct(admin) FROM money_log) ORDER BY `drop_log`.`time` DESC LIMIT 50""")
+     result = ro.fetchall()
+     
+     for ele in result:
+         print ele 
+
+
+
+def uploadAdmins(stat, ro):
+     ro.execute("""SELECT Distinct(admin) FROM money_log ORDER BY admin ASC""")
+     result = ro.fetchall()
+     
+     for ele in result:
+          try:
+               stat.execute("""INSERT INTO admins (adminName) Values (%s)""", ele[0])
+          except:
+               pass
+
+if __name__ == "__main__":
+     ro=MySQLdb.connect(host=creds.host,user=creds.drinkRoUser,
+                       passwd=creds.drinkRoPass,db=creds.drinkRoDb)
+     stats=MySQLdb.connect(host=creds.host,user=creds.drinkStatUser,
+                       passwd=creds.drinkStatPass,db=creds.drinkStatDb)
+     cursRo = ro.cursor()
+     cursStat = stats.cursor()
+     
+     uploadAdmins(cursStat, cursRo)
+     uploadDrops(cursStat, cursRo)
+
+
+
